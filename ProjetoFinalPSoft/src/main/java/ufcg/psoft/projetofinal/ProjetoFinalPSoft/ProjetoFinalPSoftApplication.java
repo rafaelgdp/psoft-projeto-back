8a38d69ae2a7c88aba9886f7b5a91bd3503aaeeb
Rafael Pontes <rafaelgpontes1@gmail.com>
2019-06-13 00:22:19 -0300

    Criando projeto spring boot

---
+++ ProjetoFinalPSoft/src/main/java/ufcg/psoft/projetofinal/ProjetoFinalPSoft/ProjetoFinalPSoftApplication.java
@@ -0,0 +1,13 @@
+package ufcg.psoft.projetofinal.ProjetoFinalPSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalPSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalPSoftApplication.class, args);
+	}
+
+}
