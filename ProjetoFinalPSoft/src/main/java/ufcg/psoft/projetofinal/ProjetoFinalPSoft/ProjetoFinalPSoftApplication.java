e6b142866a51d845cd73aa07801df0e4441d0aa2
Rafael Pontes <rafaelgpontes1@gmail.com>
2019-06-21 22:46:44 -0300

    Luta com CORS

--- ProjetoFinalPSoft/src/main/java/ufcg/psoft/projetofinal/ProjetoFinalPSoft/ProjetoFinalPSoftApplication.java
+++ ProjetoFinalPSoft/src/main/java/ufcg/psoft/projetofinal/ProjetoFinalPSoft/ProjetoFinalPSoftApplication.java
@@ -2,9 +2,20 @@ package ufcg.psoft.projetofinal.ProjetoFinalPSoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProjetoFinalPSoftApplication {
+	
+	public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/v1/auth/register").allowedOrigins("*");
+            }
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoFinalPSoftApplication.class, args);
