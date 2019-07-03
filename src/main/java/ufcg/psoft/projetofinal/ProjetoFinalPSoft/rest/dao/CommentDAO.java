0cddf65575f30111718e52a7aca355f822be08ed
Rafael Pontes <rafaelgpontes1@gmail.com>
2019-07-02 23:37:11 -0300

    tomcat added

--- src/main/java/ufcg/psoft/projetofinal/ProjetoFinalPSoft/rest/dao/CommentDAO.java
+++ src/main/java/ufcg/psoft/projetofinal/ProjetoFinalPSoft/rest/dao/CommentDAO.java
@@ -6,9 +6,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import ufcg.psoft.projetofinal.ProjetoFinalPSoft.rest.model.Comment;
-import ufcg.psoft.projetofinal.ProjetoFinalPSoft.rest.model.Course;
+
 
 
 public interface CommentDAO<T, ID extends Serializable> extends JpaRepository<Comment, Integer> {
